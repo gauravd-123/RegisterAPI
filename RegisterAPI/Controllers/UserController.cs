0265c5e6aa080c1e88482c89f7c4c2a01a8d19af
gauravd-123 <gauravdabholkar410@gmail.com>
2023-08-19 18:46:07 +0530

    Add project files.

---
+++ RegisterAPI/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RegisterAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly IConfiguration _config;
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+		[HttpPost]
+		public IActionResult Create()
+		{
+			return Ok("Create Method Running");
+		}
+    }
+}
---
+++ RegisterAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace RegisterAPI.Models
+{
+	public class User
+	{
+        public int UserID { get; set; }
+        public string? UserName { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		public string? Pwd { get; set; }
+		public int? Contact { get; set; }
+		public string? Email { get; set; }
+        public DateTime MemberSince { get; set; }
+    }
+}
